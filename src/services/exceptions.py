a8d4f4e09c8e67cd3948971bb6e71cb9b91ef41a
Dmytro Chebotarov <chebodi@ukr.net>
2024-06-28 00:25:45 +0200

    fix: project structure

--- src/services/exceptions.py
+++ src/services/exceptions.py
@@ -19,3 +19,26 @@ class DuplicateBookError(LibraryItemError):
 
 class BookNotFoundError(LibraryItemError):
     message = "Book not found in the library."
+
+
+class LibraryItemError(Exception):
+    message: str
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class BookAlreadyBorrowedError(LibraryItemError):
+    message = "This book is already borrowed."
+
+
+class BookNotBorrowedError(LibraryItemError):
+    message = "This book was not borrowed."
+
+
+class DuplicateBookError(LibraryItemError):
+    message = "This book is already exist in library."
+
+
+class BookNotFoundError(LibraryItemError):
+    message = "Book not found in the library."
--- src/services/library.py
+++ src/services/library.py
@@ -1,10 +1,6 @@
 from enum import Enum
 from dataclasses import dataclass
-from src.services.exceptions import BookAlreadyBorrowedError, \
-    BookNotBorrowedError, \
-    DuplicateBookError, \
-    BookNotFoundError, \
-    LibraryItemError
+from src.services import exceptions
 
 
 class Condition(Enum):
@@ -35,7 +31,7 @@ class Book(LibraryItem):
     def display_info(self):
         super().display_info()
         print(f"Is Borrowed: {self.is_borrowed}")
-        print(f"Condition: {self.book_condition}",'\n')
+        print(f"Condition: {self.book_condition}", '\n')
 
 
 class Library:
@@ -45,7 +41,7 @@ class Library:
     def add_items(self, item: LibraryItem):
         for existing_item in self.library:
             if existing_item.title == item.title:
-                raise DuplicateBookError
+                raise exceptions.DuplicateBookError
         self.library.append(item)
 
     def remove_items(self, title: str):
@@ -53,18 +49,18 @@ class Library:
             if item.title == title:
                 self.library.remove(item)
                 return
-        raise BookNotFoundError
+        raise exceptions.BookNotFoundError
 
     def borrow_items(self, title: str):
         for item in self.library:
             if isinstance(item, Book) and item.title == title:
                 if item.is_borrowed:
-                    raise BookAlreadyBorrowedError
+                    raise exceptions.BookAlreadyBorrowedError
 
                 else:
                     item.is_borrowed = True
                     return
-        raise BookNotFoundError
+        raise exceptions.BookNotFoundError
 
     def return_items(self, title: str):
         for item in self.library:
@@ -73,11 +69,10 @@ class Library:
                     item.is_borrowed = False
                     return
                 else:
-                    raise BookNotBorrowedError
-        raise BookNotFoundError
+                    raise exceptions.BookNotBorrowedError
+        raise exceptions.BookNotFoundError
 
     def show_available_items(self):
         for item in self.library:
             if isinstance(item, Book) and not item.is_borrowed:
                 item.display_info()
-
